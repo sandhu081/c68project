b1195fd9c655b0f528cc2035ee372c546a9237eb
sandhu081 <60472918+sandhu081@users.noreply.github.com>
2020-09-12 16:21:48 +0530

    Add files via upload

---
+++ screens/BookTransactionScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default class TransactionScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>TransactionScreen</Text>
+                </View>
+        )
+    }
+}
---
+++ screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>SearchScreen</Text>
+                </View>
+        )
+    }
+}
